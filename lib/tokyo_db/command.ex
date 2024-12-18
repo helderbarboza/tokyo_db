a7d5c7c5582cbd43d9b34c1272f0ae9f2db4a5ab
Helder Barboza <29435727+helderbarboza@users.noreply.github.com>
2024-12-18 13:20:23 -0400

    Add fun docs on Command

--- lib/tokyo_db/command.ex
+++ lib/tokyo_db/command.ex
@@ -1,8 +1,40 @@
 defmodule TokyoDB.Command do
+  @moduledoc """
+  Command decoding tools.
+  """
+
   @digits_only_pattern ~r/^\d+$/
   @unscaped_quotes_pattern ~r/((?<!\\)")/
   @tokens_pattern ~r/"([^"\\]*(\\.)?)*"|\S+/
 
+  @doc """
+  Parses a token using the corresponding type.
+
+    iex> Command.parse("TRUE")
+    true
+
+    iex> Command.parse("\\"TRUE\\"")
+    "TRUE"
+
+    iex> Command.parse("true")
+    "true"
+
+    iex> Command.parse("NIL")
+    nil
+
+    iex> Command.parse("Parkour!")
+    "Parkour!"
+
+    iex> Command.parse("Michael Scarn")
+    "Michael Scarn"
+
+    iex> Command.parse("360")
+    360
+
+    iex> Command.parse("1.61803")
+    "1.61803"
+
+  """
   @spec parse(String.t()) :: boolean() | nil | String.t() | integer()
   def parse(string)
   def parse("TRUE"), do: true
@@ -19,6 +51,19 @@ defmodule TokyoDB.Command do
     end
   end
 
+  @doc """
+  Splits a command into a list of tokens.
+
+    iex> Command.split("GET count_of_jokes_told")
+    {:ok, ["GET", "count_of_jokes_told"]}
+
+    iex> Command.split("PUT next_prank \\"put stuff on Jell-o\\"")
+    {:ok, ["PUT", "next_prank", "\\"put stuff on Jell-o\\""]}
+
+    iex> Command.split("UPDATE \\"wrong\\"quotes\\"")
+    :error
+
+  """
   @spec split(String.t()) :: {:ok, [String.t()]} | :error
   def split(string) do
     quotes_count =
@@ -26,13 +71,15 @@ defmodule TokyoDB.Command do
       |> Regex.scan(string, capture: :first)
       |> Enum.count()
 
-    if rem(quotes_count, 2) === 0 do
-      @tokens_pattern
-      |> Regex.scan(string, capture: :first)
-      |> List.flatten()
-      |> then(&{:ok, &1})
-    else
-      :error
+    case rem(quotes_count, 2) do
+      0 ->
+        @tokens_pattern
+        |> Regex.scan(string, capture: :first)
+        |> List.flatten()
+        |> then(&{:ok, &1})
+
+      _ ->
+        :error
     end
   end
 end

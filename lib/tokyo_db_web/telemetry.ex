c6c24a965478e48c4e6c152b3bd1c849d1a15615
Helder Barboza <29435727+helderbarboza@users.noreply.github.com>
2024-12-19 20:39:05 -0400

    Fix credo issue

--- lib/tokyo_db_web/telemetry.ex
+++ lib/tokyo_db_web/telemetry.ex
@@ -4,10 +4,6 @@ defmodule TokyoDBWeb.Telemetry do
   use Supervisor
   import Telemetry.Metrics
 
-  def start_link(arg) do
-    Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
-  end
-
   @impl true
   def init(_arg) do
     children = [
@@ -19,6 +15,10 @@ defmodule TokyoDBWeb.Telemetry do
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def start_link(arg) do
+    Supervisor.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
   def metrics do
